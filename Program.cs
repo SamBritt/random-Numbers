c7f784f7e50d9b1f4237f35d6955b6a3baa90efd
SamBritt <sam.britt2@yahoo.com>
2019-05-28 11:54:01 -0500

    first commit using Rando Numbers

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace randomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers.Contains(i)){
+                    Console.WriteLine($"The numbers list contains {i}");
+                }else{
+                    Console.WriteLine($"The numbers list does not contain {i}");
+                }
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
